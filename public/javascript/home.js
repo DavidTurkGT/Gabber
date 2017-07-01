831e967bf3e1f7b6c487dfb107766590d0f3381c
DavidTurkGT <dlturk747@gmail.com>
2017-07-01 08:37:11 -0400

    Clean up

--- public/javascript/home.js
+++ public/javascript/home.js
@@ -1,7 +1,4 @@
-console.log("Hello there!");
-
 function gobble(){
   let inputForm = document.querySelector(".input-form");
-  console.log("inputForm: ", inputForm);
   inputForm.classList.toggle("hidden");
 }
