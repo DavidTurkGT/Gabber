9b9b1260f55caac20095ec5834dbe164ad77d590
DavidTurkGT <dlturk747@gmail.com>
2017-07-01 08:10:51 -0400

    Added basic layout for home page

--- gobl.js
+++ gobl.js
@@ -12,6 +12,8 @@ app.engine('mustache', mustacheExpress());
 app.set('views','./views');
 app.set('view engine', 'mustache');
 
+app.use(express.static('public'));
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(validator());
--- routes/home.js
+++ routes/home.js
@@ -12,19 +12,90 @@ const isLoggedIn = (req,res,next) => {
   }
 }
 
+let User = {
+  name:"David Turk",
+  start: "Jan 1, 1970"
+};
+
+let Posts = [
+  {
+    author: "David Turk",
+    body: "This is the first post!",
+    liked: true,
+    likedBy: ["Sara", "Cornbread", "Steve Jobs"],
+    delete: true
+  },
+  {
+    author: "Cornbread",
+    body: "Meow.  Meow-meow",
+    liked: false,
+    likedBy: [],
+    delete: false
+  },
+  {
+    author: "David Turk",
+    body: "Why is my cat gobbling?",
+    liked: true,
+    likedBy: ["Sara"],
+    delete: true
+  },
+  {
+    author: "Sara",
+    body: "I have a weird cat...",
+    liked: true,
+    likedBy: ["David Turk"],
+    delete: false
+  },
+  {
+    author: "Steve Jobs",
+    body: "Why didn't I think of this?!",
+    liked: false,
+    likedBy: [],
+    delete: false
+  },
+  {
+    author: "Martha",
+    body: "Where am I?",
+    liked: true,
+    likedBy: ["Cornbread"],
+    delete: false
+  },
+  {
+    author: "David Turk",
+    body: "Total Sue move...",
+    liked: true,
+    likedBy: ["Sara", "Cornbread"],
+    delete: true
+  },
+  {
+    author: "David Turk",
+    body: "Don't like this gobble",
+    liked: false,
+    likedBy: [],
+    delete: true
+  },
+  {
+    author: "Cornbread",
+    body: "Meow",
+    liked: true,
+    likedBy: ["Martha"],
+    delete: false
+  },
+  {
+    author: "Sara",
+    body: "Last Post!",
+    liked: false,
+    likedBy: [],
+    delete: false
+  },
+];
+
 router.get("/", (req, res) => {
   res.render("gobl");
 });
 
-router.get("/:userId/:username", isLoggedIn, (req, res) => {
-  models.Users.findById(req.params.userId).then( (user) =>{
-    user = {
-      username: user.username,
-      displayname: user.displayname,
-      id: user.id
-    };
-    res.render("home", {user: user});
-  });
+router.get("/:userId/:username", (req, res) => {
+  res.render("home", {user: User, posts: Posts});
 });
 
 router.post("/login", (req, res) => {
