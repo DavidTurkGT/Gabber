7058cbb54c140e88b07bd625ccdb9ada08c776de
DavidTurkGT <dlturk747@gmail.com>
2017-07-03 17:02:25 -0400

    Styling tweaks

--- public/javascript/home.js
+++ public/javascript/home.js
@@ -1,4 +1,6 @@
 function gobble(){
   let inputForm = document.querySelector(".input-form");
   inputForm.classList.toggle("hidden");
+  let input = document.querySelector(".input-form input");
+  input.focus();
 }
