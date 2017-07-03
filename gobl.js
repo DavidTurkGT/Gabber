eed341a1ead45c1da2d7d44bee94b83f40d6cca2
DavidTurkGT <dlturk747@gmail.com>
2017-07-03 15:00:50 -0400

    Connects to Heroku DB

--- gobl.js
+++ gobl.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const validator = require('express-validator');
 const session = require('express-session');
 const morgan = require('morgan');
+const pg = require('pg');
 
 const app = express();
 
@@ -26,6 +27,18 @@ app.use(session({
   resave: false,
   saveUninitialized: false
 }));
+
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
 
 app.use(router);
 
